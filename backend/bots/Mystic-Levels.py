1e155fcab5b810233b1ff075d1a4843c467c41da
Paul Bouvier <idioticbatteringram@gmail.com>
2023-12-23 07:04:28 -0500

    Updated levels file with default config

---
+++ backend/bots/Mystic-Levels.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='-', intents=intents)
+tree = bot.tree
+
+@bot.command()
+async def syncCommand(ctx):
+    if ctx.author.id != 973834864853909525: return # if not me, Kai, exit
+    msg = await ctx.send("syncing...")             # Send sync message
+    await tree.sync()                              # sync / commands
+    await msg.edit(content="done!")                # edit the sync message to notify we are done
+
+
+@tree.command(name="rank")
+async def rankCommand(interaction:discord.Interaction):
+    pass # send pil image here
