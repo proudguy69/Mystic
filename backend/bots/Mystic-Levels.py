bf8e0cf8e3d489a8d7a442126a04a17f652abb87
Paul Bouvier <idioticbatteringram@gmail.com>
2023-12-23 07:10:46 -0500

    added bot token and settings.json

--- backend/bots/Mystic-Levels.py
+++ backend/bots/Mystic-Levels.py
@@ -1,9 +1,12 @@
 import discord
+import json
 from discord.ext import commands
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='-', intents=intents)
 tree = bot.tree
+fp = open("../settings.json")
+SETTINGS = json.load(fp)
 
 @bot.command()
 async def syncCommand(ctx):
@@ -16,3 +19,8 @@ async def syncCommand(ctx):
 @tree.command(name="rank")
 async def rankCommand(interaction:discord.Interaction):
     pass # send pil image here
+
+
+
+
+bot.run(SETTINGS["token"])
